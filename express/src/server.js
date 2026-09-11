00dd4fdf70a690632e8dae6f7d4a2896e268653d
Abdallah <uuu@email.com>
2026-09-11 22:24:16 +0300

    ready for deployment

--- express/src/server.js
+++ express/src/server.js
@@ -2,9 +2,19 @@ import dotenv from "dotenv"
 import express from "express";
 import authRouter from "./routes/auth.route.js";
 import messagesRoute from "./routes/message.route.js"
+import path from "path"
 const app = express();
 app.use("/api/auth/" , authRouter)
 app.use("/api/message/" , messagesRoute)
 dotenv.config()
+
+const __dirname = path.resolve();
+if(process.env.NODE_ENV == "production") {
+    app.use(express.static(path.join(__dirname , "../reactjs/dist")))
+
+    app.get("*" , (req , res) => {
+        res.sendFile(path.join(__dirname , "../reactjs/dist/index.html") )
+    })
+}
 
 app.listen(process.env.PORT , ()=> {})
