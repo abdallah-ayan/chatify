7ba08acfd9e99a0500bba3ef04af7ecb3027571b
Abdallah <uuu@email.com>
2026-09-15 17:15:36 +0300

    add arcjet security

---
+++ express/src/lib/arcject.js
@@ -0,0 +1,20 @@
+import arcjet, { shield, detectBot, tokenBucket , slidingWindow } from "@arcjet/node";
+import "dotenv/config"
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE"]
+    }),
+    slidingWindow({
+      mode : "LIVE" ,
+      max : 100 ,
+      interval : 60 // s
+    })
+  ],
+});
+
+export default aj 
--- express/src/server.js
+++ express/src/server.js
@@ -7,8 +7,11 @@ import path from "path"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
 import errorController from "./controller/error.Controller.js";
+import { arcjetProduction } from "./middleware/arcjet.middleware.js";
 dotenv.config()
 const app = express();
+
+app.use(arcjetProduction);
 app.use(express.json({limit : "10kb"}));
 app.use(cookieParser())
 app.use("/api/auth/" , authRouter)
