9e686ded2729a0df10a78b0c3a3e2fa90b530553
Abdallah <uuu@email.com>
2026-09-16 22:14:26 +0300

    add signup page

---
+++ reactjs/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import { OrbitProgress } from "react-loading-indicators";
+
+export default function Loading() {
+  return (
+    <OrbitProgress
+      color="#449db3"
+      size="medium"
+      text=""
+      textColor=""
+    />
+  );
+}
